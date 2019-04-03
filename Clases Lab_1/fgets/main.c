5f1721f08b5fe140844b50bf08332c93e1a72108
gilberto <calderongilberto3@gmail.com>
2019-04-03 11:39:05 -0300

    ...

---
+++ Clases Lab_1/fgets/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char buffer[64];
+    int cantidad;
+
+    printf("Nombre: ");
+    //fgets(buffer, 62, stdin);
+    fgets(buffer,sizeof(buffer)-2,stdin);
+
+    cantidad = strlen(buffer);
+
+    buffer[cantidad-1] = '\0';
+
+    printf("%s", buffer);
+
+    return 0;
+}
