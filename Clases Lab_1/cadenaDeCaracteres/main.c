14722fc8631792b062bbd3da8c15bc0f4cefbc7c
Gilberto <calderongilberto3@gmail.com>
2019-04-10 01:11:14 -0300

    //

--- Clases Lab_1/cadenaDeCaracteres/main.c
+++ Clases Lab_1/cadenaDeCaracteres/main.c
@@ -6,16 +6,27 @@ int main()
 {
     //string
 
-    char cadena[50];                                                    // en un vector char tengo un caracter
+    char cadena[50];
                                                                         // en string al final esta el elemento \0
-    fflush(stdin);                                                      //49 letras
+    //fflush(stdin);                                                      //49 letras
     //scanf("%s", cadena);  no te toma el espacio
     //gets(cadena); // no existe en linux , te tooma el espacio           // no se pone amperson sino el nombre del vector
     // no se recomienda el uso de gets
     //fgets(); // te añade un
-    printf("%s",cadena);                                               // cadena es la direccion de memoria del primer indice  //cadena == &cadena[10]
+    //printf("%s",cadena);                                               // cadena es la direccion de memoria del primer indice  //cadena == &cadena[10]
 
 
+   /*
+   fgets
+
+   int c;                                                // en un vector char tengo un caracter
+    printf("Ingrese un nombre: ");
+    fgets(cadena , sizeof(cadena)-2, stdin);
+
+    c = strlen(cadena);
+    cadena[c-1] = '\0';
+
+    printf("%s", cadena);*
 
 
     return 0;
--- Clases Lab_1/fgets/main.c
+++ Clases Lab_1/fgets/main.c
@@ -12,8 +12,11 @@ int main()
     fgets(buffer,sizeof(buffer)-2,stdin);
 
     cantidad = strlen(buffer); //cantidad de elementos de una cadena
+  //  printf("cantidad : %d\n", cantidad);
 
     buffer[cantidad-1] = '\0';
+
+
 
     printf("%s", buffer);
 
